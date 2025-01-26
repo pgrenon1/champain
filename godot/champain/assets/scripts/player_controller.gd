aa0df5de5630691d613a64396ee7675867efd899
Thomas Gauthier-Caron <thomas.gauthier.c@gmail.com>
2025-01-25 21:29:40 -0500

    player_controller mouse debug

--- godot/champain/assets/scripts/player_controller.gd
+++ godot/champain/assets/scripts/player_controller.gd
@@ -16,27 +16,33 @@ func _ready() -> void:
 	pass
 	
 func _physics_process(delta: float) -> void:
-	var player_pointing_angle = ControllerManager.instance.get_player_angle(player_id)
-	$debug.global_rotation = player_pointing_angle
+	if ControllerManager.instance:
+		var direction = Vector2.ZERO
+		var player_pointing_angle
+		
+		if use_mouse:
+			direction = position - get_viewport().get_mouse_position()
+			direction = direction.normalized()
+			
+			player_pointing_angle = (-direction).angle()
+		else:
+			player_pointing_angle = ControllerManager.instance.get_player_angle(player_id)
+		$debug.global_rotation = player_pointing_angle
 
-	var direction = Vector2.ZERO
-	
-	var boost = false
-	
-	if use_mouse:
-		direction = position - get_viewport().get_mouse_position()
-		direction = direction.normalized()
 		
-		if Input.is_action_just_pressed("move" + str(player_id+1)):
-			boost = true
-	else:
-		direction = -Vector2.from_angle(player_pointing_angle)
+		var boost = false
 		
-		if ControllerManager.instance.get_shake_down(player_id):
-			boost = true
-	
-	if boost:
-		apply_impulse(direction * speed)
+		if use_mouse:
+			if Input.is_action_just_pressed("move" + str(player_id+1)):
+				boost = true
+		else:
+			direction = -Vector2.from_angle(player_pointing_angle)
+			
+			if ControllerManager.instance.get_shake_down(player_id):
+				boost = true
+		
+		if boost:
+			apply_impulse(direction * speed)
 
 
 func validate_checkpoint(checkpoint_id: int):
