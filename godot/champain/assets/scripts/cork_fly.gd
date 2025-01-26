d0ac89d911cb3bea44e123ed86c09e682a721471
Thomas Gauthier-Caron <thomas.gauthier.c@gmail.com>
2025-01-26 16:30:03 -0500

    cork fly!

---
+++ godot/champain/assets/scripts/cork_fly.gd
@@ -0,0 +1,49 @@
+extends Sprite2D
+
+@export var launch_speed = 0.0
+
+var _initial_position = Vector2.ZERO
+var _launched_duplicate: Sprite2D = null
+
+@export_category("Debug")
+@export var launch_button: bool:
+	set(value):
+		launch()
+		
+@export_category("Debug")
+@export var reset_button: bool:
+	set(value):
+		reset()
+
+func launch():
+	if _launched_duplicate != null:
+		return
+		
+	# Create new Sprite2D and copy properties
+	_launched_duplicate = Sprite2D.new()
+	_launched_duplicate.texture = texture
+	_launched_duplicate.scale = scale
+	_launched_duplicate.offset = offset
+	_launched_duplicate.flip_h = flip_h
+	_launched_duplicate.flip_v = flip_v
+	
+	# Set up movement script
+	_launched_duplicate.set_script(preload("res://assets/scripts/projectile.gd"))
+	get_tree().root.add_child(_launched_duplicate)
+	_launched_duplicate.global_transform = global_transform
+	
+	# Setup duplicate's movement
+	_launched_duplicate.launch_speed = launch_speed
+	_launched_duplicate.launch()
+	
+	visible = false
+	print("Dly")    
+		
+func _ready():
+	_initial_position = global_position
+
+func reset():
+	if _launched_duplicate:
+		_launched_duplicate.queue_free()
+		_launched_duplicate = null
+	visible = true
