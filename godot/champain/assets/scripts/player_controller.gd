6e9dccbc97561368211c009e49a2f0e75570ace5
Thomas Gauthier-Caron <thomas.gauthier.c@gmail.com>
2025-01-25 19:37:01 -0500

    use mouse or motion

--- godot/champain/assets/scripts/player_controller.gd
+++ godot/champain/assets/scripts/player_controller.gd
@@ -2,6 +2,8 @@ class_name Player extends RigidBody2D
 
 @export var speed = 400
 @export var player_id = 0
+@export var use_mouse = false
+
 var lap_count = 0
 var validated_checkpoints = []
 
@@ -13,14 +15,26 @@ func _ready() -> void:
 func _physics_process(delta: float) -> void:
 	var player_pointing_angle = ControllerManager.instance.get_player_angle(player_id)
 	$debug.global_rotation = player_pointing_angle
-	#var direction = position - get_viewport().get_mouse_position()
-	#direction = direction.normalized()
+
+	var direction = Vector2.ZERO
 	
-	direction = Vector2.from_angle(player_pointing_angle)
+	var boost = false
 	
-	#if Input.is_action_just_pressed("move" + str(player_id+1)):
-	if ControllerManager.instance.get_shake_down(player_id):
-		apply_impulse(-direction * speed)
+	if use_mouse:
+		direction = position - get_viewport().get_mouse_position()
+		direction = direction.normalized()
+		
+		if Input.is_action_just_pressed("move" + str(player_id+1)):
+			boost = true
+	else:
+		direction = -Vector2.from_angle(player_pointing_angle)
+		
+		if ControllerManager.instance.get_shake_down(player_id):
+			boost = true
+	
+	if boost:
+		apply_impulse(direction * speed)
+
 
 func validate_checkpoint(checkpoint_id: int):
 	if !validated_checkpoints.has(checkpoint_id):
