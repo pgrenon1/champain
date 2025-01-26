9d6b8a104f482b6943d8cfd62d9affbdd9e5bcb9
Thomas Gauthier-Caron <thomas.gauthier.c@gmail.com>
2025-01-25 19:26:39 -0500

    motion controls!

--- godot/champain/assets/scripts/player_controller.gd
+++ godot/champain/assets/scripts/player_controller.gd
@@ -4,14 +4,22 @@ extends RigidBody2D
 @export var player_id = 0
 var validated_checkpoints = []
 
+var player_controller
+
 func _ready() -> void:
 	pass
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(delta: float) -> void:
+	var player_pointing_angle = ControllerManager.instance.get_player_angle(player_id)
+	$debug.global_rotation = player_pointing_angle
 	var direction = position - get_viewport().get_mouse_position()
 	direction = direction.normalized()
-	if Input.is_action_just_pressed("move" + str(player_id)):
-		apply_impulse(direction * speed)
+	
+	direction = Vector2.from_angle(player_pointing_angle)
+	
+	#if Input.is_action_just_pressed("move" + str(player_id+1)):
+	if ControllerManager.instance.get_shake_down(player_id):
+		apply_impulse(-direction * speed)
 	pass
 
 func validate_checkpoint(id: int):
---
+++ godot/champain/assets/scripts/where_controllers_live.gd
@@ -0,0 +1,104 @@
+class_name ControllerManager
+extends Node
+
+static var instance: ControllerManager = null
+
+var players = {}  # Dictionary for player tracking
+var players_angles = {}  # Dictionary for angles
+var player_ids = []  # List to maintain player IDs in order
+
+# Input state tracking
+var _current_frame_shakes = {}  # Shakes that happened this frame
+var _last_frame_shakes = {}    # Shakes that happened last frame
+var _shake_timestamps = {}     # Last shake timestamp per player
+
+func _ready():
+	self.instance = self
+	
+	
+func clear_logic():
+	# await end of frame to clear logic
+	await get_tree().process_frame
+	_last_frame_shakes = _current_frame_shakes.duplicate()
+	_current_frame_shakes.clear()
+
+func _process(_delta):
+	self.clear_logic.call_deferred()
+
+# Input interface methods
+static func get_shake_down(player_num: int) -> bool:
+
+	if not is_instance_valid(instance):
+		return false
+	
+	if player_num >= instance.player_ids.size():
+		return false
+		
+
+	var player_id = instance.player_ids[player_num]
+	
+	return player_id in instance._current_frame_shakes
+
+static func get_shake_up(player_num: int) -> bool:
+	if not is_instance_valid(instance):
+		return false
+	
+	if player_num >= instance.player_ids.size():
+		return false
+		
+	var player_id = instance.player_ids[player_num]
+	return player_id in instance._last_frame_shakes and not player_id in instance._current_frame_shakes
+
+static func get_shake(player_num: int) -> bool:
+	if not is_instance_valid(instance):
+		return false
+	
+	if player_num >= instance.player_ids.size():
+		return false
+		
+	var player_id = instance.player_ids[player_num]
+	return player_id in instance._last_frame_shakes
+
+static func get_angle(player_num: int) -> float:
+	if not is_instance_valid(instance):
+		return 0.0
+	return instance.get_player_angle(player_num)
+
+func _on_quaternion(player_id, quat):
+	if not player_id in players:
+		players[player_id] = true
+		players_angles[player_id] = 0.0
+		if not player_id in player_ids:
+			player_ids.append(player_id)
+	
+	var rotated_UP = quat * Vector3.UP
+	var normal = Vector3(0, 0, 1)
+	var projected = rotated_UP - rotated_UP.project(normal)
+	projected = Vector2(projected.x, projected.y)
+	
+	players_angles[player_id] = -projected.angle()
+
+func get_player_angle(player_num: int) -> float:
+	if player_num < 0:
+		return 0.0
+	
+	if player_num >= player_ids.size():
+		return 0.0
+		
+	var player_id = player_ids[player_num]
+	return players_angles.get(player_id, 0.0)
+
+func _on_shake(player_id, timestamp):
+	_current_frame_shakes[player_id] = true
+	_shake_timestamps[player_id] = timestamp
+
+func _on_node_2d_message_received(address, value, time):
+	if address == '/quaternion':
+		var player_id = value[0]
+		var quaternion = Quaternion(value[1], value[2], value[3], value[4])
+		_on_quaternion(player_id, quaternion)
+		
+	if address == '/shake':
+		var player_id = value[0]
+		var timestamp = value[1].to_int()
+		_on_shake(player_id, timestamp)
