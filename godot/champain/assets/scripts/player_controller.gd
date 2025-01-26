2119c6f208767d6f1777e6f75ed154ba140e3a26
Thomas Gauthier-Caron <thomas.gauthier.c@gmail.com>
2025-01-25 22:07:30 -0500

    removed print

--- godot/champain/assets/scripts/player_controller.gd
+++ godot/champain/assets/scripts/player_controller.gd
@@ -35,10 +35,7 @@ func _process(delta: float) -> void:
 func _physics_process(delta: float) -> void:
 	if not (ControllerManager.instance != null):
 		return
-	
-	if ControllerManager.instance.get_pop_down(player_id):
-		print(true)
-	
+
 	var player_pointing_angle
 	if use_mouse:
 		var mouse_pos = get_viewport().get_mouse_position()
